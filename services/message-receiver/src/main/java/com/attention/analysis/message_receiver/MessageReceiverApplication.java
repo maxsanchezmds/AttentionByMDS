b17df06a57dd0948cc300ac92532cd712e076a72
maxsanchezmds <maximilianosanchez1304@gmail.com>
2025-05-01 23:13:25 -0400

    1.0.1 MessageReceiverApplication v1

--- services/message-receiver/src/main/java/com/attention/analysis/message_receiver/MessageReceiverApplication.java
+++ services/message-receiver/src/main/java/com/attention/analysis/message_receiver/MessageReceiverApplication.java
@@ -2,10 +2,9 @@ package com.attention.analysis.message_receiver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication(exclude = {ContextRegionProviderAutoConfiguration.class})
+@SpringBootApplication
 @EnableAsync
 public class MessageReceiverApplication {
     public static void main(String[] args) {

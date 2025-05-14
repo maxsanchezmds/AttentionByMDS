da918298dc0af2c8c3ffc39485675d034f41691b
maxsanchezmds <maximilianosanchez1304@gmail.com>
2025-05-14 19:53:18 -0400

    7.1.0 AccessService en blanco

---
+++ services/Access-Service/src/main/java/com/attention/analysis/Access_Service/AccessServiceApplication.java
@@ -0,0 +1,13 @@
+package com.attention.analysis.Access_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessServiceApplication.class, args);
+	}
+
+}

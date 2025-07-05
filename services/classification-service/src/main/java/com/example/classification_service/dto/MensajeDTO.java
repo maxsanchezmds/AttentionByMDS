b46eedc90e3329ef4535f63674f319e57778089e
maxsanchezmds <maximilianosanchez1304@gmail.com>
2025-07-05 12:40:04 -0400

    v7.7.24

--- services/classification-service/src/main/java/com/example/classification_service/dto/MensajeDTO.java
+++ services/classification-service/src/main/java/com/example/classification_service/dto/MensajeDTO.java
@@ -1,9 +1,11 @@
 package com.example.classification_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MensajeDTO {
     private Long id;
     private String mensaje;
@@ -13,6 +15,7 @@ public class MensajeDTO {
     private ConversacionDTO conversacion;
     
     @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class ConversacionDTO {
         private Long id;
         private LocalDateTime fechaCreacion;
